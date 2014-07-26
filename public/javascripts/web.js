1c657f7032ba9be9cd4522dccf2ccf7afffb0907
Amol Borcar & Rj Bernaldo <pair+amolborcar.rjbernaldo@devbootcamp.com>
2014-07-25 18:03:45 -0700

    Created JavaScript skeleton
    Connected to Twitter streaming API.  Set up main controller.  Set up MVC globe and MVC db.  TODO: connect to database, interact with DOM.

--- public/javascripts/web.js
+++ public/javascripts/web.js
@@ -2,6 +2,8 @@ window.onload = function() {
 	var socket = io.connect('http://localhost')
 
 	socket.on('new message', function(data) {
-		console.log(data)
+    var div = document.createElement('div')
+    div.innerHTML = data.text
+		document.body.appendChild(div);
 	});
 }
