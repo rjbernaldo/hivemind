cce97b2b2a537ff38a9f4b9c6a6ac29f158cdea2
Sherwood Callaway <shcallaway@gmail.com>
2014-07-27 11:49:19 -0700

    Added line graph view to the database controller and globe view to the globe controller.

--- mainController.js
+++ mainController.js
@@ -12,11 +12,13 @@ var consumer_secret = "zUHEYmnHhlSQwQoS8PczLYKOBnrVQErjJzJ0et6DgZMT4jw78W"
 var access_token_key = "2678182524-WsHJJGoyzQft2XpBcPNu4ByGmWYkl4F89ELYApX"
 var access_token_secret = "4U2HJbntXykhMhLQyAptI5RRXVoWrrltOCnsne3aKpgEt"
 var db = mongojs('mydb')
-var HOUR = 3600000
-var DAY = 86400000
+var MS_HOUR = 3600000,
+    MS_DAY = 86400000;
+var io;
 // var counter = 0;
 
-module.exports = function() {
+module.exports = function(io) {
+  io = io;
   var master_controller = new MasterController;
   master_controller.connect()
   master_controller.stream()
@@ -50,24 +52,34 @@ MasterController.prototype = {
 }
 
 function GlobeController() {
-  // this.view = new GlobeView;
+  this.view = new GlobeView;
 }
 
 GlobeController.prototype = {
   extractCoordinates: function(tweet) {
     if (tweet.coordinates) {
-      return tweet.coordinates.coordinates;
+      this.view.chartCoordinates(tweet.coordinates.coordinates);
     }
   }
 }
 
-function DatabaseController() {}
+function GlobeView() {}
+
+GlobeView.prototype = {
+  chartCoordinates: function(coordinates) {
+    // chart coordinates on globe
+  }
+}
+
+function DatabaseController() {
+  this.line_graph_view = new LineGraphView;
+}
 
 DatabaseController.prototype = {
   removeDeprecatedCounts: function() {
     setInterval(function() {
       db.collection('counts').remove({value: 1});
-    }, HOUR);
+    }, MS_HOUR);
   },
   removeDeprecatedTweets: function() {
     db.collection('hashtags').remove({timestamp: {"$lt": Date.now() - DAY}})
@@ -95,44 +107,10 @@ DatabaseController.prototype = {
   }
 }
 
+function LineGraphView() {}
 
-// var dbController = (function(view, model){
-//   // Remove hashtags where count = 1 every hour
-//   setInterval(removeOldHashtagCounts, HOUR)
-
-//   function removeOldHashtagCounts(){
-//     db.collection('hashtagCount').remove({ value: 1 })
-//   }
-
-//   return {
-//     view: view,
-//     model: model,
-//     parseRawTweet: function(io, tweet){
-//       var timestamp = tweet.created_at;
-
-
-//       if (tweet.entities && tweet.entities.hashtags.length > 0) {
-//         tweet.entities.hashtags.forEach(function(tag) {
-//           db.collection('tweets').insert( {created_at: timestamp, hashtag: tag.text, timestamp: Date.parse(timestamp) } );
-
-//           function map() { emit( this.hashtag, 1 ) }
-//           function reduce(key, values) { return Array.sum(values) }
-//           function finalize(key, value) { return { value: value, time: Date.now() } }
-
-//         db.collection('tweets').mapReduce(map, reduce, {
-//                         query: { hashtag: tag.text },
-//                         out: { merge: "hashtagCount" }
-//                         // finalize: finalize
-//                         });
-//         // Remove tweets that came in more than 24 hours ago
-//         db.collection('tweets').remove( { timestamp: { "$lt": Date.now() - DAY } } )
-
-//         // DO NOT DELETE - find the top 5 most used hashtags in database
-//         // db.hashtagCount.find( { $query: {}, $orderby: { value: -1 } } ).limit(5)
-//         });
-//       }
-//     }
-//   }
-
-// })(dbView, dbModel);
-
+LineGraphView.prototype = {
+  draw: function() {
+   // draw line graph
+  }
+}
