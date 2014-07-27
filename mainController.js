fa98a5b87469406a0eac687a7674e5943913d32b
Amol Borcar & Sherwood Callaway <pair+amolborcar.shcallaway@devbootcamp.com>
2014-07-27 12:27:58 -0700

    Made sure that updateCounts, storeHashtags, extractHashtags, removeDeprecatedHashtags, and removeDeprecatedCounts methods are all operationg correctly.

--- mainController.js
+++ mainController.js
@@ -1,21 +1,15 @@
-var Twitter = require('twitter')
-var mongojs = require('mongojs')
-// var consumer_key = process.env.CONSUMER_KEY
-// var consumer_secret = process.env.CONSUMER_SECRET
-// var access_token_key = process.env.ACCESS_TOKEN_KEY
-// var access_token_secret = process.env.ACCESS_TOKEN_SECRET
-// var dbuser = process.env.DBUSER
-// var dbpass = process.env.DBPASSWORD
-// var db = mongojs('mongodb://'+dbuser+':'+dbpass+'@ds059908.mongolab.com:59908/livedata')
-var consumer_key = "i1k8pKNzfJLMi8d9F6ISbs7SV"
-var consumer_secret = "zUHEYmnHhlSQwQoS8PczLYKOBnrVQErjJzJ0et6DgZMT4jw78W"
-var access_token_key = "2678182524-WsHJJGoyzQft2XpBcPNu4ByGmWYkl4F89ELYApX"
-var access_token_secret = "4U2HJbntXykhMhLQyAptI5RRXVoWrrltOCnsne3aKpgEt"
-var db = mongojs('mydb')
+var Twitter = require('twitter'),
+    mongojs = require('mongojs')
+var consumer_key = process.env.CONSUMER_KEY,
+    consumer_secret = process.env.CONSUMER_SECRET,
+    access_token_key = process.env.ACCESS_TOKEN_KEY,
+    access_token_secret = process.env.ACCESS_TOKEN_SECRET
+var dbuser = process.env.DBUSER,
+    dbpass = process.env.DBPASSWORD,
+    db = mongojs('mongodb://'+dbuser+':'+dbpass+'@ds059908.mongolab.com:59908/livedata')
 var MS_HOUR = 3600000,
     MS_DAY = 86400000;
 var io;
-// var counter = 0;
 
 module.exports = function(io) {
   io = io;
@@ -23,7 +17,6 @@ module.exports = function(io) {
   master_controller.connect()
   master_controller.stream()
 }
-
 
 function MasterController() {
   this.API = null;
@@ -81,8 +74,8 @@ DatabaseController.prototype = {
       db.collection('counts').remove({value: 1});
     }, MS_HOUR);
   },
-  removeDeprecatedTweets: function() {
-    db.collection('hashtags').remove({timestamp: {"$lt": Date.now() - DAY}})
+  removeDeprecatedHashtags: function() {
+    db.collection('hashtags').remove({timestamp: {"$lt": Date.now() - MS_DAY}})
   },
   extractHashtags: function(tweet) {
     if (tweet.entities && tweet.entities.hashtags.length > 0) {
@@ -92,10 +85,11 @@ DatabaseController.prototype = {
   storeHashtags: function(tweet) {
     for (var i = 0; i < tweet.entities.hashtags.length; i++) {
       db.collection('hashtags').insert({created_at: tweet.created_at,
-        hashtag: tweet.entities.hashtags[i].text, timestamp: Date.parse(tweet.created_at)};
+                                        hashtag: tweet.entities.hashtags[i].text,
+                                        timestamp: Date.parse(tweet.created_at)});
       this.updateCounts(tweet.entities.hashtags[i]);
     }
-    this.removeDeprecatedTweets();
+    this.removeDeprecatedHashtags();
   },
   updateCounts: function(hashtag) {
     function map() {emit(this.hashtag, 1)}
