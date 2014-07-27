8a9f063815aaee7a8062003326a01994f65f2067
Sherwood Callaway <shcallaway@gmail.com>
2014-07-26 18:08:08 -0700

    Modularized the methods that interact with the twitter stream into a single object called the master controller. Also modularized the methods involved with parsing out coordinates into a globe controller. Began modularizing the methods involved with parsing out and storing hashtags in the database into a database controller.

--- mainController.js
+++ mainController.js
@@ -1,89 +1,81 @@
 var Twitter = require('twitter')
 var mongojs = require('mongojs')
-var consumer_key = process.env.CONSUMER_KEY
-var consumer_secret = process.env.CONSUMER_SECRET
-var access_token_key = process.env.ACCESS_TOKEN_KEY
-var access_token_secret = process.env.ACCESS_TOKEN_SECRET
-var dbuser = process.env.DBUSER
-var dbpass = process.env.DBPASSWORD
-var db = mongojs('mongodb://'+dbuser+':'+dbpass+'@ds059908.mongolab.com:59908/livedata')
+// var consumer_key = process.env.CONSUMER_KEY
+// var consumer_secret = process.env.CONSUMER_SECRET
+// var access_token_key = process.env.ACCESS_TOKEN_KEY
+// var access_token_secret = process.env.ACCESS_TOKEN_SECRET
+// var dbuser = process.env.DBUSER
+// var dbpass = process.env.DBPASSWORD
+// var db = mongojs('mongodb://'+dbuser+':'+dbpass+'@ds059908.mongolab.com:59908/livedata')
+var consumer_key = "i1k8pKNzfJLMi8d9F6ISbs7SV"
+var consumer_secret = "zUHEYmnHhlSQwQoS8PczLYKOBnrVQErjJzJ0et6DgZMT4jw78W"
+var access_token_key = "2678182524-WsHJJGoyzQft2XpBcPNu4ByGmWYkl4F89ELYApX"
+var access_token_secret = "4U2HJbntXykhMhLQyAptI5RRXVoWrrltOCnsne3aKpgEt"
+var db = mongojs('mydb')
 var HOUR = 3600000
 var DAY = 86400000
 // var counter = 0;
 
 // log the tweets
-module.exports = streamTweets;
+module.exports = initialize;
 
-// TWITTER STREAM
-function streamTweets(io) {
-  connectToTwitter().stream('filter', { 'locations': '-180,-90,180,90' }, function(stream) {
-    stream.on('data', function(data) {
-      sendToGlobeController(io, data);
-      sendToDBController(io, data);
+function initialize() {
+  var master_controller = new MasterController;
+  master_controller.connect()
+  master_controller.stream()
+}
+
+function MasterController() {
+  this.API = null;
+  this.globe_controller = new GlobeController;
+  this.database_controller = new DatabaseController;
+}
+
+MasterController.prototype = {
+  connect: function() {
+    this.API = new Twitter({
+      consumer_key: consumer_key,
+      consumer_secret: consumer_secret,
+      access_token_key: access_token_key,
+      access_token_secret: access_token_secret
     });
-  });
+  },
+  stream: function() {
+    this.API.stream('filter', {'locations': '-180,-90,180,90'}, function(stream) {
+      stream.on('data', function(data) {
+        this.globe_controller.extractCoordinates(data);
+        this.database_controller.extractHashtags(data);
+        // dbController.parseRawTweet(null, data)
+      }.bind(this));
+    }.bind(this));
+  }
 }
 
-// HELPERS
-function connectToTwitter(){
-  var twitter = new Twitter({
-    consumer_key: consumer_key,
-    consumer_secret: consumer_secret,
-    access_token_key: access_token_key,
-    access_token_secret: access_token_secret
-  });
-  return twitter
+function GlobeController() {
+  // this.view = new GlobeView;
 }
 
-function sendToGlobeController(io, data) {
-  // TODO require globeController
-  globeController.parseCoords(io, data);
-}
-
-function sendToDBController(io, data) {
-  // TODO: require DBController
-  dbController.parseRawTweet(io, data);
-}
-
-// Globe View
-var globeView = (function() {
-  return {
-    renderOnGlobe: function(coords){
-      // counter++;
+GlobeController.prototype = {
+  extractCoordinates: function(data) {
+    if (data.coordinates) {
+      return data.coordinates.coordinates;
     }
   }
-})();
+}
 
-// Globe Controller
-var globeController = (function(view){
-  this.view = view;
+function DatabaseController() {}
 
-  function sendToGlobeView(coords){
-    this.view.renderOnGlobe(coords);
+DatabaseController.prototype = {
+  extractHashtags: function(data) {
+
+  },
+  removeDeprecated: function() {
+    setInterval(function() {
+      db.collection('hashtagCount').remove({value: 1});
+    }, HOUR);
   }
+}
 
-  return {
-    parseCoords: function(io, data){
-      if (data.coordinates){
-        // TODO send to view (emit)
-        sendToGlobeView(data.coordinates.coordinates)
-      }
-    }
-  }
-})(globeView);
-
-// db view
-var dbView = (function(){
-
-})();
-
-// db Model
-
-var dbModel = (function(){
-
-})();
-
-// db Controller
 
 var dbController = (function(view, model){
   // Remove hashtags where count = 1 every hour
@@ -123,17 +115,4 @@ var dbController = (function(view, model){
   }
 
 })(dbView, dbModel);
-/*
-function() {
-  db.collection('hashtagCount').findAndModify({
-    query: { _id: tag.text },
-    update: {
-      $addToSet: { value: 1 }
-    },
-    new: true,
-    upsert: true
-  });
-};
-
-*/
 
