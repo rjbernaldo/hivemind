7660b1f8f65f567ad47c678e793aaadb45f20f1f
Rj <rj.bernaldo@gmail.com>
2014-07-28 19:18:30 -0700

    More globe changes. Refactored a bit, transformed lines to circles

--- public/globe/globeUI.js
+++ public/globe/globeUI.js
@@ -13,43 +13,32 @@ if(!Detector.webgl){
 	// 	new TWEEN.Tween(globe).to({time: 0},500).easing(TWEEN.Easing.Cubic.EaseOut).start();
 	// };
 
-	TWEEN.start();
+	// TWEEN.start();
 	window.onload = function() {
-		var collection = [];
 		document.body.style.backgroundImage = 'none';
 
+		var hashtagCounter = 0;
 		var socket = io.connect('http://localhost');
-
-		// UI.TweetCount.init();
-  //   UI.HashtagCount.init();
 
 		socket.on('newGlobeTweet', function(data) {
 			var longtitude = data[1];
 			var latitude = data[0];
-			var magnitude = 0.9;
-			var coord = [longtitude, latitude, magnitude];
-
-			collection.push(longtitude);
-			collection.push(latitude);
-			collection.push(magnitude);
 
 			UI.TweetCount.update();
 
-			renderPoints();
-
+			renderPoints(longtitude, latitude);
 		});
-		var hashtagCounter = 0;
 
 		socket.on('newHashtag', function(data){
 			UI.HashtagCount.update(data);
 		});
 
-		function renderPoints() {
-			globe.addData(collection, {format: 'magnitude', name: "tweets", animated: true});
-			collection = []
-			globe.createPoints();
-		}
 		globe.animate();
 	}
+}
 
+function renderPoints(longtitude, latitude) {
+	var collection = [longtitude, latitude, 0.9];
+	globe.addData(collection, {format: 'magnitude', name: "tweets", animated: true});
+	globe.createPoints();
 }
