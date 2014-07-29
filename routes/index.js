40155747016d3529ac50d2e87d56257ab71d4a71
Rj <rj.bernaldo@gmail.com>
2014-07-29 15:54:19 -0700

    Moved files in the globe directory to their proper place

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var express = require('express'),
     path = require('path');
 
 router.get('/', function(request, response) {
-  // var globeIndex = path.join(__dirname, 'index.html');
-  response.sendfile('index.html');
+  var globeIndex = path.join(__dirname, '../views/index.html');
+  response.sendfile(globeIndex);
 });
 
 router.get('/hashtag', function(request, response) {
