be9eff4edb8cfa0112bbc7bcdf405a14e3ef621f
Matias Meneses <matiasmeneses@Matiass-MacBook-Pro.local>
2014-07-29 19:45:28 -0700

    adding changes to index styling

---
+++ public/javascripts/globe-indexUI.js
@@ -0,0 +1,122 @@
+var UI = {};
+UI.TweetCount = (function() {
+  var tweetCounter = 0;
+
+  function counterCount() {
+    return tweetCounter++;
+  }
+
+  return {
+    update: function() {
+      var currentCount = counterCount();
+      UI.View.updateTweetCount(currentCount);
+    }
+  }
+})();
+
+UI.HashtagCount = (function() {
+  var hashtagCounter = 0;
+
+  function counterCount(data) {
+    return hashtagCounter += data;
+  }
+
+  return {
+    update:function(data) {
+      var currentCount = counterCount(data);
+      UI.View.updateHashtagCount(currentCount);
+    }
+
+  }
+})();
+
+UI.SessionTimer = (function(){
+
+  return {
+    update:function(seconds) {
+      UI.View.updateTimeCount(seconds)
+    }
+  }
+})();
+
+UI.View = (function() {
+  var tweetCountDom = document.getElementsByClassName('tweetCount');
+  var hashtagCountDom = document.getElementsByClassName('hashtagCount');
+  var tweetTimeCountDom = document.getElementsByClassName('tweetCountDescription')
+  // var countryCountDom = document.getElementsByClassName('something');
+
+  function animateDescription() {
+    $('#watching').toggle(
+      function(){
+        $('#what').addClass('flipOutX')
+        $('#what').css('display','none')
+        $('#explanation').css('display','block')
+        $('#explanation').removeClass('flipOutX')
+        $('#explanation').addClass('flipInX')        
+        },
+      function(){
+        $('#explanation').removeClass('flipInX')
+        $('#explanation').addClass('flipOutX')
+        setTimeout(function(){
+          $('#what').css('display','block')
+          $('#what').removeClass('flipOutX')
+          $('#what').addClass('flipInX')
+        }, 500)
+      }
+    )
+  }
+
+  function hoverTweetCount() {
+    $('.tweetCountContainer').hover(
+      function() {
+        $('.tweetCountDescription').css('display','block')
+        $('.tweetCountDescription').removeClass('flipOutX')
+        $('.tweetCountDescription').addClass('flipInX')
+      },
+      function() {
+        $('.tweetCountDescription').removeClass('flipInX')
+        $('.tweetCountDescription').addClass('flipOutX')
+        setTimeout(function(){
+          $('.tweetCountDescription').css('display','none')
+        }, 500)
+      }
+    )
+  }
+
+  function hoverHashtagCount() {
+    $('.hashtagCountContainer').hover(
+      function() {
+        $('.hashtagCountDescription').css('display','block')
+        $('.hashtagCountDescription').removeClass('flipOutX')
+        $('.hashtagCountDescription').addClass('flipInX')
+      },
+      function() {
+        $('.hashtagCountDescription').removeClass('flipInX')
+        $('.hashtagCountDescription').addClass('flipOutX')
+        setTimeout(function(){
+          $('.hashtagCountDescription').css('display','none')
+        }, 500)
+      }
+    )
+  }
+
+  return {
+    init: function() {
+      animateDescription();
+      hoverTweetCount();
+      hoverHashtagCount();
+      // initializeCountryCount();
+    },
+    updateTweetCount: function(count) {
+      tweetCountDom[0].innerHTML = count;
+    },
+    updateHashtagCount: function(data) {
+      hashtagCountDom[0].innerHTML = data
+    },
+    updateTimeCount: function(count) {
+      tweetTimeCountDom[0].innerHTML = 'The amount of tweets streamed in the last ' + count + ' seconds.'
+    }
+  }
+})();
+
+UI.View.init();
