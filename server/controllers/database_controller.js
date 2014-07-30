6ee16945ea42da34d9a7d67f3a20f2d65c91b23e
Sherwood Callaway <shcallaway@gmail.com>
2014-07-30 12:46:43 -0700

    Decoupled database controller from the database model.

--- server/controllers/database_controller.js
+++ server/controllers/database_controller.js
@@ -1,48 +1,21 @@
-var dbuser = process.env.DBUSER,
-    dbpass = process.env.DBPASSWORD,
-    mongojs = require('mongojs'),
-    db = mongojs('mongodb://' + dbuser + ':' + dbpass + '@ds059908.mongolab.com:59908/livedata');
-var MS_HOUR = 3600000,
-    MS_DAY = 86400000;
-var LineGraphView = require('../views/line_graph_view');
+var LineGraphView = require('../views/line_graph_view'),
+    Database = require('../models/database');
 
-function DatabaseController(io) {this.line_graph_view = new LineGraphView(io);}
+function DatabaseController(io) {
+  this.line_graph_view = new LineGraphView(io);
+  this.database = new Database;
+}
 DatabaseController.prototype = {
-  removeDeprecatedCounts: function() {
-    setInterval(function() {
-      db.collection('counts').remove({value: 1});
-    }, MS_HOUR);
+  setupDatabase: function() {
+    this.database.setupIndicies();
+    this.database.removeDeprecatedCounts();
   },
-  removeDeprecatedHashtags: function() {
-    db.collection('hashtags').remove({timestamp: {"$lt": Date.now() - MS_DAY}})
+  passTweetToDatabase: function(tweet) {
+    this.database.extractHashtagsFromTweet(tweet);
   },
-  extractHashtags: function(tweet) {
-    if (tweet.entities && tweet.entities.hashtags.length > 0) {
-      this.storeHashtags(tweet);
-    }
-  },
-  storeHashtags: function(tweet) {
-    for (var i = 0; i < tweet.entities.hashtags.length; i++) {
-      db.collection('hashtags').insert({created_at: tweet.created_at,
-                                        hashtag: tweet.entities.hashtags[i].text,
-                                        timestamp: Date.parse(tweet.created_at)});
-      this.updateCounts(tweet.entities.hashtags[i]);
-    }
-    this.removeDeprecatedHashtags();
-  },
-  updateCounts: function(hashtag) {
-    function map() {emit(this.hashtag, 1)}
-    function reduce(key, values) {return Array.sum(values)}
-    db.collection('hashtags').mapReduce(map, reduce, {
-      query: {hashtag: hashtag.text},
-      out: {merge: "counts"}
-    });
-  },
-  calculateTopFiveHashtags: function() {
-    var query = db.collection('counts').find({}).sort({value: -1}).limit(5)
-    query.toArray(function(error, topFiveHashtagCounts){
-      this.line_graph_view.draw(topFiveHashtagCounts);
-    }.bind(this));
+  passTopHashtagCountsToLineGraph: function() {
+    var topFiveHashtagCounts = this.database.calculateTopFiveHashtags();
+    this.line_graph_view.update(topFiveHashtagCounts);
   }
 }
 
---
+++ server/models/database.js
@@ -0,0 +1,58 @@
+var dbuser = process.env.DBUSER,
+    dbpass = process.env.DBPASSWORD,
+    mongojs = require('mongojs'),
+    db = mongojs(process.env.DATABASE);
+var MS_HOUR = 3600000,
+    MS_DAY = 86400000;
+
+function Database() {}
+
+Database.prototype = {
+  extractHashtagsFromTweet: function(tweet) {
+    if (tweet.entities && tweet.entities.hashtags.length > 0) {
+      this.storeHashtags(tweet);
+    }
+  },
+  setupIndicies: function() {
+    db.collection('hashtags').createIndex({'hashtag': 1});
+    db.collection('hashtags').createIndex({'timestamp': 1});
+    db.collection('counts').createIndex({'value': -1});
+  },
+  removeDeprecatedCounts: function() {
+    setInterval(function() {
+      db.collection('counts').remove({value: 1});
+    }, MS_HOUR);
+  },
+  removeDeprecatedHashtags: function() {
+    db.collection('hashtags').remove({timestamp: {"$lt": Date.now() - MS_DAY}})
+  },
+  storeHashtags: function(tweet) {
+    for (var i = 0; i < tweet.entities.hashtags.length; i++) {
+      db.collection('hashtags').insert({created_at: tweet.created_at,
+                                        hashtag: tweet.entities.hashtags[i].text,
+                                        timestamp: Date.parse(tweet.created_at)});
+      this.updateCounts(tweet.entities.hashtags[i]);
+    }
+    this.removeDeprecatedHashtags();
+  },
+  updateCounts: function(hashtag) {
+    function map() {emit(this.hashtag, 1)}
+    function reduce(key, values) {return Array.sum(values)}
+    db.collection('hashtags').mapReduce(map, reduce, {
+      query: {hashtag: hashtag.text},
+      out: {merge: "counts"}
+    });
+  },
+  calculateTopFiveHashtags: function() {
+    var cursor = db.collection('counts').find({}).sort({value: -1}).limit(5)
+    var query_results = []
+    cursor.forEach(function(document) {
+      console.log(document)
+      // query_results.push(document);
+    })
+    // console.log(query_results);
+    return query_results;
+  }
+}
+
+module.exports = Database;
